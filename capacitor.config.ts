bb7e14e04e57f0a73b5238acfb4269e15f0308b4
burunuu <btezori@gmail.com>
2026-09-08 23:20:41 -0400

    Configura Capacitor do RotazRO Entregador

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rotazro.entregador',
+  appName: 'RotazRO Entregador',
+  webDir: 'dist'
+};
+
+export default config;
