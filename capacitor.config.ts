2ea5bb705cfe8387e61b4c5a42c8c2187b32520a
burunuu <btezori@gmail.com>
2026-09-10 22:49:16 -0400

    Configura background geolocation no app Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.rotazro.entregador',
   appName: 'RotazRO Entregador',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  android: {
+    useLegacyBridge: true,
+  },
 };
 
 export default config;
